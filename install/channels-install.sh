c47f4db7baeb9216f8d3d52d17b06bbc334147e4
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2024-04-12 12:00:42 +1000

    [Channels] Remove intel specific HW-accel package

--- install/channels-install.sh
+++ install/channels-install.sh
@@ -26,8 +26,7 @@ if [[ "$CTTYPE" == "0" ]]; then
   msg_info "Setting Up Hardware Acceleration"
   $STD apt-get -y install \
     va-driver-all \
-    ocl-icd-libopencl1 \
-    intel-opencl-icd
+    ocl-icd-libopencl1
   chgrp video /dev/dri
   chmod 755 /dev/dri
   chmod 660 /dev/dri/*
