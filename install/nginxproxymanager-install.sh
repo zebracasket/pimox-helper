c052788716f0543032f0670ccdc0d9814b753dc7
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2024-04-13 19:46:25 +1000

    [NginxPM & MotionEye] Fix pip issues

--- install/nginxproxymanager-install.sh
+++ install/nginxproxymanager-install.sh
@@ -40,6 +40,7 @@ $STD apt-get install -y \
   python3-cffi \
   python3-certbot \
   python3-certbot-dns-cloudflare
+rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
 $STD pip3 install certbot_dns_porkbun
 $STD pip3 install certbot-dns-duckdns
 $STD python3 -m venv /opt/certbot/
