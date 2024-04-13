332945abb3c0f0a3379bd1f0a51e2b0b3dfa3f77
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2024-04-13 21:08:55 +1000

    [Paperless-ngx] Fix pip issues

--- install/paperless-ngx-install.sh
+++ install/paperless-ngx-install.sh
@@ -82,6 +82,7 @@ $STD tar -xf paperless-ngx-$Paperlessngx.tar.xz -C /opt/
 mv paperless-ngx paperless
 rm paperless-ngx-$Paperlessngx.tar.xz
 cd /opt/paperless
+rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
 $STD pip install --upgrade pip
 $STD pip install -r requirements.txt
 curl -s -o /opt/paperless/paperless.conf https://raw.githubusercontent.com/paperless-ngx/paperless-ngx/main/paperless.conf.example
