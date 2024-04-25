75cb19bd1218be42c629a343a242c6a8ebadec9d
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2024-04-25 11:38:30 +1000

    [Traccar] add wget to deps

--- install/traccar-install.sh
+++ install/traccar-install.sh
@@ -17,6 +17,7 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
+$STD apt-get install -y wget
 msg_ok "Installed Dependencies"
 
 RELEASE=$(curl -s https://api.github.com/repos/traccar/traccar/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
